82d69143bd8c4f3c8020517c6da3f424b2d79059
Filip <filip.milak@pinecone.hr>
2020-03-30 09:57:13 +0200

    lint -> needs fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { useContext } from "react";
+import "./App.css";
 import RootStore from "./RootStore";
 import AppRouter from "./AppRouter";
 
-export const RootStoreContext = React.createContext(new RootStore());
+const RootStoreContext = React.createContext(new RootStore());
+
+export const rootContext = (): RootStore => useContext(RootStoreContext);
 
 const App: React.FC = () => {
-  return (
-      <RootStoreContext.Provider value={new RootStore()}>
-        <AppRouter />
-      </RootStoreContext.Provider>
-
-  );
+  return <AppRouter />;
 };
 
 export default App;
--- src/login/LoginView.tsx
+++ src/login/LoginView.tsx
@@ -1,22 +1,24 @@
-import React, {useContext} from "react";
-import {Button, Form, Icon, Input} from "antd";
-import '../App.css'
-import {RootStoreContext} from "../App";
-import {observer} from "mobx-react";
+import {Button, Form, Icon, Input} from 'antd';
+import {observer} from 'mobx-react';
+import React from 'react';
+import {rootContext} from '../App';
+import '../App.css';
 
 const LoginView: React.FC<any> = observer(() => {
-    const rootStore = useContext(RootStoreContext);
-    const {loginStore} = rootStore;
+    const {loginStore} = rootContext();
 
     return (
         <Form>
             <Form.Item>
-                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                       placeholder="Username" onChange={loginStore.onUsernameChange} />
+                <Input
+                    prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}} />}
+                    placeholder="Username"
+                    onChange={loginStore.onUsernameChange}
+                />
             </Form.Item>
             <Form.Item>
                 <Input
-                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}} />}
                     type="password"
                     placeholder="Password"
                     onChange={loginStore.onPasswordChange}
@@ -26,7 +28,7 @@ const LoginView: React.FC<any> = observer(() => {
                 <Button onClick={loginStore.tryLogin}>Log in</Button>
             </Form.Item>
         </Form>
-    )
+    );
 });
 
 export default LoginView;
